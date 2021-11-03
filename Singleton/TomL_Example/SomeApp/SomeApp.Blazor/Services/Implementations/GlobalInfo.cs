0b7c99a91c02a003bda8b1ef0d823da086bceed8
Thomas Lachin <thomas.lachin@relativity.com>
2021-11-03 08:16:35 -0500

    Added my example which is the modern approach using containers.  Built in VS 2022 preview, and is a Blazor App.

---
+++ Singleton/TomL_Example/SomeApp/SomeApp.Blazor/Services/Implementations/GlobalInfo.cs
@@ -0,0 +1,58 @@
+﻿using SomeApp.Blazor.Services.Interfaces;
+using SomeApp.Blazor.Services.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace SomeApp.Blazor.Services.Implementations
+{
+	public sealed class GlobalInfo : IGlobalInfo
+	{
+		private List<User> _usersOnline;
+
+		public GlobalInfo()
+		{
+			_usersOnline = new List<User>();
+		}
+
+		public async Task<bool> AddUserOnlineAsync(User user)
+		{
+			bool result = false;
+
+			if(!_usersOnline.Contains(user))
+			{
+				_usersOnline.Add(user);
+				result = true;
+			}
+
+			return await Task.FromResult(result);
+		}
+
+		public async Task<bool> RemoveUserOnlineAsync(User user)
+		{
+			bool result = false;
+			if (_usersOnline.Contains(user))
+			{
+				_usersOnline.Remove(user);
+				result = true;
+			}
+
+			return await Task.FromResult(result);
+		}
+
+		public async Task<User> GetLastUserOnlineAsync()
+		{
+			return await Task.FromResult(_usersOnline.Last());
+		}
+
+		public async Task<List<User>> GetAllUsersOnlineAsync()
+		{
+			return await Task.FromResult(_usersOnline);
+		}
+
+		public async Task<int> GetCurrentUserCountAsync()
+		{
+			return await Task.FromResult(_usersOnline.Count);
+		}
+	}
+}
