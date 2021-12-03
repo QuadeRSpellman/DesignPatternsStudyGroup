9ba714a4d1be53cf336d3951483e455f1f7fed67
Thomas Lachin <thomas.lachin@relativity.com>
2021-12-03 11:09:19 -0600

    Command pattern version of my blazor website

---
+++ Command/TomL_Example/SomeApp/SomeApp.Blazor/Services/Implementations/AddUserCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using SomeApp.Blazor.Services.Interfaces;
+using SomeApp.Blazor.Services.Models;
+
+namespace SomeApp.Blazor.Services.Implementations
+{
+		public class AddUserCommand : ICommand
+		{
+				private readonly IGlobalInfo _globalInfo;
+				private readonly User _user;
+
+				public AddUserCommand(IGlobalInfo globalInfo, User user)
+				{
+						_globalInfo = globalInfo;
+						_user = user;
+				}
+
+				public bool CanExecute()
+				{
+						return _user != null;
+				}
+
+				public void Execute()
+				{
+						_globalInfo.AddUserOnlineAsync(_user).Wait();
+				}
+
+				public void Undo()
+				{
+						_globalInfo.RemoveUserOnlineAsync(_user).Wait();
+				}
+		}
+}
---
+++ Command/TomL_Example/SomeApp/SomeApp.Blazor/Services/Interfaces/IGlobalInfo.cs
@@ -0,0 +1,15 @@
+﻿using SomeApp.Blazor.Services.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace SomeApp.Blazor.Services.Interfaces
+{
+	public interface IGlobalInfo
+	{
+		Task<bool> AddUserOnlineAsync(User user);
+		Task<bool> RemoveUserOnlineAsync(User user);
+		Task<User> GetLastUserOnlineAsync();
+		Task<List<User>> GetAllUsersOnlineAsync();
+		Task<int> GetCurrentUserCountAsync();
+	}
+}
